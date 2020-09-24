21ef8411d4e6da1d08d69eba2b58e3baadcada97
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-09-24 17:39:47 +0900

    useContext

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { UserDispatch } from './AppReducer';
+import useInputs from './useInputs';
 
-function CreateUser({ username, email, onChange, onCreate }) {
+const CreateUser = () => {
+
+    const [{username, email}, onChange, reset] = useInputs({
+        username: '',
+        email: '',
+    });
+
+    const nextId = useRef(4);
+    const dispatch = useContext(UserDispatch);
+
+    const onCreate = () => {
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email,
+            }
+        });
+        reset();
+        nextId.current += 1;
+    };
+
     return (
         <div>
             <input 
@@ -18,7 +42,7 @@ function CreateUser({ username, email, onChange, onCreate }) {
             <button onClick={onCreate}>등록</button>
         </div>
         
-    )
-}
+    );
+};
 
 export default React.memo(CreateUser);
