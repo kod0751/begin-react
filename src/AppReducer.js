58277ea287e30536d7165ff0b6e661971a75b9d6
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-09-25 22:51:33 +0900

    immer

--- src/AppReducer.js
+++ src/AppReducer.js
@@ -1,7 +1,8 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
+import produce from 'immer';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
-import useInputs from './useInputs';
+
 
 function countActiveUsers(users) {
   console.log('활성사용자 수를 세는 중...');
@@ -34,26 +35,35 @@ const initialState = {
 function reducer(state, action) {
     switch (action.type) {
         case 'CREATE_USER':
-            return {
-                inputs: initialState.inputs,
-                users: state.users.concat(action.user)
-            };
+            return produce(state, draft => {
+                draft.users.push(action.user);
+            })
+            // return {
+            //     users: state.users.concat(action.user)
+            // };
         case 'TOGGLE_USER':
-            return {
-                ...state,
-                users: state.users.map( user =>
-                    user.id === action.id
-                    ? {...user, active: !user.active}
-                    : user
-                    )
-            };
+            return produce(state, draft => {
+                const user = draft.users.find(user => user.id === action.id);
+                user.active = !user.active;
+            })
+            // return {
+            //     ...state,
+            //     users: state.users.map( user =>
+            //         user.id === action.id
+            //         ? {...user, active: !user.active}
+            //         : user
+            //         )
+            // };
         case 'REMOVE_USER':
-            return {
-                ...state,
-                users: state.users.filter( user =>
-                    user.id !== action.id
-                    )
-            };
+            return produce(state, draft => {
+               const index = draft.users.findIndex( user => user.id === action.id);
+               draft.users.splice(index, 1);
+            })
+            // return {
+            //     users: state.users.filter( user =>
+            //         user.id !== action.id
+            //         )
+            // };
         default:
             throw new Error('Unhandled action');
     };
